20146bf18ff07c022ea0e334e18774396d25a5fa
nada-challenge <nadachallenge.web@outlook.jp>
2026-06-13 11:36:24 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/nada-challenge-2026/',
-  build: {
-    rollupOptions: {
-      input: {
-        main:     'index.html',
-        about:    'about.html',
-        parade:   'parade.html',
-        spot:     'spot.html',
-        access:   'access.html',
-        donation: 'donation.html',
-        404:      '404.html',
-      }
-    }
-  }
 })
